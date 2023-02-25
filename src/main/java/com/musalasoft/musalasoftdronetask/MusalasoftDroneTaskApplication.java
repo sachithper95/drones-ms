af76bb8178387ac652d17ab6aca8ee7fba1d8b0a
sran0732 <sachith.rangana@syscolabs.com>
2023-02-26 05:20:55 +0530

    Initial commit

---
+++ src/main/java/com/musalasoft/musalasoftdronetask/MusalasoftDroneTaskApplication.java
@@ -0,0 +1,13 @@
+package com.musalasoft.musalasoftdronetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusalasoftDroneTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusalasoftDroneTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musalasoft/musalasoftdronetask/MusalasoftDroneTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalasoft.musalasoftdronetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusalasoftDroneTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
